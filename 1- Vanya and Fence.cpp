3b325df66c4d2f55a86de1b6b7f7cd19ae9600b3
Abdalla7Ahmed <abdallahahmed17120@gmail.com>
2023-03-10 18:24:00 +0200

    Problem Solving

---
+++ 1- Vanya and Fence.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "time.h"
+using namespace std;
+
+
+
+/*
+====================== A. Vanya and Fence ====================
+time limit per test 1 second
+memory limit per test 256 megabytes
+input standard input
+output standard output
+
+Vanya and his friends are walking along the fence of height h and they do not want the guard to notice them. In order to achieve this the height of each of the friends should not exceed h. If the height of some person is greater than h he can bend down and then he surely won't be noticed by the guard. The height of the i-th person is equal to ai.
+
+Consider the width of the person walking as usual to be equal to 1, while the width of the bent person is equal to 2. Friends want to talk to each other while walking, so they would like to walk in a single row. What is the minimum width of the road, such that friends can walk in a row and remain unattended by the guard?
+
+Input
+The first line of the input contains two integers n and h (1 ≤ n ≤ 1000, 1 ≤ h ≤ 1000) — the number of friends and the height of the fence, respectively.
+
+The second line contains n integers ai (1 ≤ ai ≤ 2h), the i-th of them is equal to the height of the i-th person.
+
+Output
+Print a single integer — the minimum possible valid width of the road.
+*/
+
+int main()
+{
+    int fence_h, number_of_persons;
+    int result ;
+    int flag=0;
+    int width;
+    cin>>number_of_persons>>fence_h;
+    for (int i = 0 ; i <number_of_persons ;i++)
+    {
+        cin >> result;
+        if(result > fence_h) flag++;
+    }
+    width = number_of_persons + flag;
+    cout <<width<<endl;
+    return 0;
+}
---
+++ 4-Team.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+/*
+=====================A. Team =============================
+One day three best friends Petya, Vasya and Tonya decided to form a team and take part in programming contests. Participants are usually offered several problems during programming contests. Long before the start the friends decided that they will implement a problem if at least two of them are sure about the solution. Otherwise, the friends won't write the problem's solution.
+
+This contest offers n problems to the participants. For each problem we know, which friend is sure about the solution. Help the friends find the number of problems for which they will write a solution.
+
+Input
+The first input line contains a single integer n (1 ≤ n ≤ 1000) — the number of problems in the contest. Then n lines contain three integers each, each integer is either 0 or 1. If the first number in the line equals 1, then Petya is sure about the problem's solution, otherwise he isn't sure. The second number shows Vasya's view on the solution, the third number shows Tonya's view. The numbers on the lines are separated by spaces.
+
+Output
+Print a single integer — the number of problems the friends will implement on the contest.
+*/
+
+int main()
+{
+    int number_of_problems,Flag,arr[3],counter=0;
+    cin>>number_of_problems;
+    for(int i =0 ;i<number_of_problems;i++)
+    {
+        cin >>arr[0]>>arr[1]>>arr[2];
+        if(arr[0]==1)Flag++;
+        if(arr[1]==1)Flag++;
+        if(arr[2]==1)Flag++;
+
+        if(Flag >=2)counter ++;
+        Flag=0;
+    }
+    cout<<counter<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
